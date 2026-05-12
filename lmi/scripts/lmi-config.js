3799801d39100de3436eaf058be624f3a89979b4
James <68837571+BrokenSynapse@users.noreply.github.com>
2026-05-12 02:38:43 -0400

    Add files via upload

---
+++ lmi/scripts/lmi-config.js
@@ -0,0 +1,11 @@
+window.LMI_CONFIG = {
+  appName: 'Leviathan Military Interlink',
+  manifestPath: 'module-manifest.json',
+  localKeys: {
+    relayUrl: 'LMI_RELAY_URL',
+    relaySavedAt: 'LMI_RELAY_SAVED_AT',
+    user: 'LMI_LAST_USER',
+    installed: 'LMI_INSTALLED_APPS',
+    layout: 'LMI_DESKTOP_LAYOUT'
+  }
+};
